e5cc2dc95d792622a8255a50dd36411e7f225ec0
nmd <you@example.com>
2016-10-25 16:27:15 +0700

    fix

--- resources/views/site/common/top.blade.php
+++ resources/views/site/common/top.blade.php
@@ -1,3 +1,49 @@
+<?php $device = getDevice(); ?>
+@if($device == MOBILE)
+<header>
+	<div class="mobile-topbar">
+		<div class="row">
+			<div class="small-3 columns">
+				<a class="mobile-menuopen" data-toggle="mobile-menubox" aria-controls="mobile-menubox" aria-haspopup="true" tabindex="0"><i class="fa fa-bars" aria-hidden="true"></i></a>
+			</div>
+			<div class="small-9 columns">
+				<div class="logo">
+					<a href="/">
+						<span class="logo-color-1">GAME</span>
+						<span class="logo-color-2">OFFLINE</span>
+						<span class="logo-color-3">HAY</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="column">
+			<div class="mobile-search">
+				<form action="{{ route('site.search') }}" method="GET" class="search-form">
+					<input name="name" type="text" value="" class="search-input" placeholder="Tìm kiếm">
+					<a class="search-button" onclick="$('.search-form').submit()"><i class="fa fa-search" aria-hidden="true"></i></a>
+				</form>
+			</div>
+		</div>
+	</div>
+</header>
+<div class="full reveal mobile-menubox" id="mobile-menubox" data-reveal>
+	<div class="mobile-menubox-head">
+		<strong class="mobile-title mmh-left">Danh mục</strong>
+		<a class="mmh-right" data-close aria-label="Close modal"><i class="fa fa-times" aria-hidden="true"></i> Đóng Menu</a>
+		<div class="clearfix"></div>
+	</div>
+	<ul class="mobile-menu">
+		@if($mobilemenu)
+      		@foreach($mobilemenu as $key => $value)
+				<li {{ checkCurrent(url($value->url)) }}><a href="{{ $value->url }}">{!! $value->name !!}</a></li>
+			@endforeach
+		@endif
+		<li class="mobile-li-close"><a class="mobile-menuclose" data-close aria-label="Close modal"><i class="fa fa-times" aria-hidden="true"></i> Đóng Menu</a></li>
+  	</ul>
+</div>
+@else
 <header class="show-for-medium">
 	<div class="row column">
 		<a href="/" class="logo"><img src="/img/logo.png" alt="" /></a>
@@ -17,3 +63,4 @@
         </form>
 	</div>
 </div>
+@endif
--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -22,51 +22,21 @@
    }(document, 'script', 'facebook-jssdk'));
 </script>
 @endif
-
-<div class="off-canvas-wrapper">
-	<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-		<div class="off-canvas position-left offcanvas" id="offCanvasLeft" data-off-canvas data-position="left" aria-hidden="true">
-			<button class="close-button" aria-label="Close menu" type="button" data-close="">
-	        	<span aria-hidden="true">×</span>
-	      	</button>
-	      	<ul class="mobile-ofc vertical menu menu-mobile">
-	      		<li class="title">Menu</li>
-	      		@if($mobilemenu)
-		      		@foreach($mobilemenu as $key => $value)
-						<li {{ checkCurrent(url($value->url)) }}><a href="{{ $value->url }}">{!! $value->name !!}</a></li>
-					@endforeach
-				@endif
-	      	</ul>
-		</div>
-		<div class="off-canvas-content" data-off-canvas-content>
-			<div class="title-bar hide-for-medium">
-				<div class="title-bar-left">
-					<button class="menu-icon" type="button" data-open="offCanvasLeft" aria-expanded="false" aria-controls="offCanvasLeft"><span class="menu-button-text">&nbsp;</span></button>
-      				<span class="title-bar-title">gameoffline</span>
-				</div>
-				<div class="title-bar-right">
-					<button onclick="searchhide()" class="search-icon"></button>
-				</div>
-			</div>
-			<div class="main">
-				@include('site.common.top')
-				@include('site.common.ad', ['posPc' => 1, 'posMobile' => 2])
-				<div class="row">
-					<div class="medium-8 columns">
-						<div class="content">
-							@yield('content')
-						</div>
-					</div>
-					<div class="medium-4 columns">
-						@include('site.common.side')
-					</div>
-				</div>
-				@include('site.common.ad', ['posPc' => 3, 'posMobile' => 4])
-				@include('site.common.bottom')
-			</div>
+			
+@include('site.common.top')
+@include('site.common.ad', ['posPc' => 1, 'posMobile' => 2])
+<div class="row">
+	<div class="medium-8 columns">
+		<div class="content">
+			@yield('content')
 		</div>
 	</div>
+	<div class="medium-4 columns">
+		@include('site.common.side')
+	</div>
 </div>
+@include('site.common.ad', ['posPc' => 3, 'posMobile' => 4])
+@include('site.common.bottom')
 
 <script src="{{ asset('js/app.js') }}"></script>
 </body>
