fd967129350d7789bfb967099d2914f287604271
nmd <you@example.com>
2016-11-23 09:25:52 +0700

    fix

--- resources/views/admin/common/inputContent.blade.php
+++ resources/views/admin/common/inputContent.blade.php
@@ -17,7 +17,7 @@
 </div>
 <div class="form-group">
 	<label for="image">Hình đại diện</label>
-	<p>Định dạng jpg, jpeg, png. Tên thư mục & ảnh phải là tiếng việt không dấu, không chứa dấu cách + kí tự đặc biệt. Dung lượng ảnh nhẹ (< 1mb)<br>Auto crop: 130x160</p>
+	<p>Định dạng jpg, jpeg, png. Tên thư mục & ảnh phải là tiếng việt không dấu, không chứa dấu cách + kí tự đặc biệt. Dung lượng ảnh nhẹ (< 1mb)<br>Auto crop: 170x210</p>
 	<div class="row">
 		<div class="col-sm-6">
 			<input name="image" type="text" value="{{ old('image') }}" class="form-control" readonly id="url_abs" onchange="GetFilenameFromPath2('url_abs', 1);">
@@ -47,7 +47,7 @@
 </div>
 <div class="form-group">
 	<label for="image">Hình đại diện</label>
-	<p>Định dạng jpg, jpeg, png. Tên thư mục & ảnh phải là tiếng việt không dấu, không chứa dấu cách + kí tự đặc biệt. Dung lượng ảnh nhẹ (< 1mb)<br>Auto crop: 130x160</p>
+	<p>Định dạng jpg, jpeg, png. Tên thư mục & ảnh phải là tiếng việt không dấu, không chứa dấu cách + kí tự đặc biệt. Dung lượng ảnh nhẹ (< 1mb)<br>Auto crop: 170x210</p>
 	<div class="row">
 		<div class="col-sm-6">
 			<input name="image" type="text" value="{{ $data->image }}" class="form-control" readonly id="url_abs" onchange="GetFilenameFromPath2('url_abs', 1);">
