1edfac7afbdf7828cab91e07a7690274ac5873a5
Mad Raon <muadauyeu24@gmail.com>
2017-06-27 12:34:39 +0700

    fix

--- resources/views/site/game/play.blade.php
+++ resources/views/site/game/play.blade.php
@@ -76,7 +76,7 @@
 			<div class="info">
 				<div class="row">
 					<div class="column description">{!! $game->description !!}</div>
-					<div class="column description">{!! $game->download !!}</div>
+					<!-- <div class="column description">{{-- $game->download --}}</div> -->
 					<div class="column description">
 						@include('site.common.social')
 					</div>
